e6d035110a1c43ece1980f0c0808d01ff68ab3c1
Ipshita Chakraborty <ipshita.chakraborty2211@gmail.com>
2021-12-16 17:43:50 +0530

    Exercise 1.4

--- part 1/courseinfo/src/Content.js
+++ part 1/courseinfo/src/Content.js
@@ -3,6 +3,7 @@ import Part from "./Part"
 const Content = (props) => {
     return (
         <>
+            {/* {console.log(props.parts[0].exercises)} */}
             <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
             <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
             <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
