b532d3046cd9934b571b62fb3c4087f53077306b
Ipshita Chakraborty <ipshita.chakraborty2211@gmail.com>
2021-12-26 12:08:12 +0530

    Hands-on (part 1.d) Function Returns a Function

--- followingcourse/overall/src/App.js
+++ followingcourse/overall/src/App.js
@@ -43,6 +43,7 @@ const App = () => {
   const [ left, setLeft ] = useState(0)
   const [ right, setRight ] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [ value, setValue ] = useState(10)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
@@ -53,6 +54,16 @@ const App = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
   }
+
+  const hello = (who) => {
+    const handler = () => console.log('hello ', who)
+    return handler
+  }
+
+  const setToValue = (newValue) => () => {
+    setValue(newValue)
+  }
+
   const name = "Peter"
   const age = 10
 
@@ -69,6 +80,13 @@ const App = () => {
       <Display counter={right} />
       <Button handleClick={handleRightClick} text='right' />
       <History allClicks={allClicks} />
+
+      <hr />
+
+      {value}
+      <button onClick={setToValue(1000)} >1000</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </>
   )
 }
