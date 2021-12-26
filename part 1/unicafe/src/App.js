8061b25983f540728bdc6b56347f58f3c3f84dfc
Ipshita Chakraborty <ipshita.chakraborty2211@gmail.com>
2021-12-26 19:46:49 +0530

    Exercise 1.7.0

--- part 1/unicafe/src/App.js
+++ part 1/unicafe/src/App.js
@@ -10,18 +10,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allReviews, setAllReviews] = useState(0)
 
   const setToGood = () => {
+    setAllReviews(allReviews + 1)
     setGood(good + 1)
   }
 
   const setToNeutral = () => {
+    setAllReviews(allReviews + 1)
     setNeutral(neutral + 1)
   }
 
   const setToBad = () => {
+    setAllReviews(allReviews + 1)
     setBad(bad + 1)
   }
+
+  const average = (good - bad) / allReviews
+  const positive = (good / allReviews) * 100
+
 
   return (
     <div>
@@ -33,6 +41,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {allReviews}</p>
+      <p>average {allReviews === 0 ? 0 : average}</p>
+      <p>positive {allReviews === 0 ? 0 : positive} %</p>
     </div>
   )
 }
