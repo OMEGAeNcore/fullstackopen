1d5cd0e4680c7107721f17970cd2cff26feeadce
Ipshita Chakraborty <ipshita.chakraborty2211@gmail.com>
2023-08-21 03:24:02 +0530

    part 2-c and 2-d extracting communication WIP

--- followingcourse/notes/src/App.js
+++ followingcourse/notes/src/App.js
@@ -1,51 +1,72 @@
-import React, { useState } from "react";
-import Note from './components/Note'
+import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import noteService from './services/notes'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const hook = () => {
+    console.log("effect");
+    noteService.getAll.then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  };
+
+  useEffect(hook, []);
+  console.log("render", notes.length, "notes");
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1
-    }
+    };
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-    
-  }
+    noteService.create(noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
+  };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const toggleImportance = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, importance: !note.importance}
+
+    noteService.update(url,changedNote).then(response => {
+      setNotes(notes.map(n => n.id!==id ? n: response.data))
+    })
   }
 
   return (
     <>
       <h1>Notes</h1>
       <div>
-        <button onClick={() => setShowAll(!showAll)} >Show {showAll? 'important': 'all'} </button>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? "important" : "all"}{" "}
+        </button>
       </div>
       <ul>
-        {
-          notesToShow.map(note => 
-              <Note key={note.id} note={note} />
-            )
-        }
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportance(note.id)}/>
+        ))}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">Save</button>
       </form>
     </>
-  )
-}
+  );
+};
 
 export default App;
