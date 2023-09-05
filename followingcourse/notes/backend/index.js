c17a231b2cff4d0fd3a307bc612510c8a79aeaef
Ipshita Chakraborty <ipshita.chakraborty2211@gmail.com>
2023-09-05 20:00:41 +0530

    Chapter 4 setup

--- followingcourse/notes/backend/index.js
+++ followingcourse/notes/backend/index.js
@@ -1,48 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const PORT = process.env.PORT || 3001;
-const Note = require("./mongoose/models/note");
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static("build"));
-
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
-
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
-});
-
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  });
-  note.save().then((result) => {
-    response.json(result);
-  });
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}...`);
-});
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
--- followingcourse/notes/backend/mongo.js
+++ followingcourse/notes/backend/mongo.js
@@ -1,14 +1,15 @@
 const Note = require('./mongoose/schema/Note')
+const logger = require('./utils/logger')
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
-const password = process.argv[2];
-const url = `mongodb+srv://omegaen:${password}@fullstackopen-1.oriiabj.mongodb.net/noteApp?retryWrites=true&w=majority`;
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+const password = process.argv[2]
+const url = `mongodb+srv://omegaen:${password}@fullstackopen-1.oriiabj.mongodb.net/noteApp?retryWrites=true&w=majority`
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 // const note = new Note({
 //   content: "Node.js is working",
@@ -20,10 +21,9 @@ mongoose.connect(url);
 //   mongoose.connection.close();
 // });
 
-
-Note.find({important: false}).then((result) => {
+Note.find({ important: false }).then((result) => {
   result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+    logger.info(note)
+  })
+  mongoose.connection.close()
+})
