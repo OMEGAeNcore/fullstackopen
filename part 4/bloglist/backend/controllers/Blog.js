72209d71fc95a2ad769e4db5260ee4b4e35bcaec
Ipshita Chakraborty <ipshita.chakraborty2211@gmail.com>
2023-09-08 00:06:22 +0530

    Exercise 4.13-4.14

--- part 4/bloglist/backend/controllers/Blog.js
+++ part 4/bloglist/backend/controllers/Blog.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router()
 require('express-async-errors')
 const Blog = require('../model/Blog')
+const mongoose = require('mongoose')
 
 // Get routes
 blogRouter.get('/', async (request, response) => {
@@ -15,8 +16,10 @@ blogRouter.get('/', async (request, response) => {
 // Post routes
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  if(!body.title || !body.url){
-    return response.status(400).end()
+  if (!body.title || !body.url) {
+    return response
+      .status(400)
+      .json({ error: 'Title or Url of the data are missing.' })
   }
 
   const blog = new Blog({
@@ -28,6 +31,54 @@ blogRouter.post('/', async (request, response) => {
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
+})
+
+// Delete route
+blogRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(400).json({ message: 'Id is not correct' })
+  }
+
+  const deletedBlog = await Blog.findByIdAndRemove(id)
+  if (deletedBlog) {
+    response.status(204).json(deletedBlog)
+  } else {
+    response
+      .status(400)
+      .json({ error: 'Data was already deleted from server.' })
+  }
+})
+
+// Update route
+blogRouter.put('/:id', async (request, response) => {
+  const id = request.params.id
+  const body = request.body
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(400).json({ message: 'Id is not correct' })
+  }
+  if (!body.likes && !body.author) {
+    return response.status(400).json({ error: 'Nothing to update!' })
+  }
+
+  const fetchExistingBlog = await Blog.findById(id)
+
+  if (fetchExistingBlog) {
+    const updatedBlog = {
+      title: fetchExistingBlog.title,
+      author: body.author || fetchExistingBlog.author,
+      url: fetchExistingBlog.url,
+      likes: body.likes,
+    }
+
+    const updateBlog = await Blog.findByIdAndUpdate(id, updatedBlog, {
+      new: true,
+    })
+
+    response.status(200).json({ updatedBlog: updateBlog })
+  } else {
+    response.status(404).json({ error: 'No such data found.' })
+  }
 })
 
 module.exports = blogRouter
--- part 4/bloglist/backend/tests/test_blogs_api.test.js
+++ part 4/bloglist/backend/tests/test_blogs_api.test.js
@@ -8,83 +8,145 @@ const api = supertest(app)
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-
-  let blogObject = helper.initBlogs.map((blog) => new Blog(blog))
-  const promiseArray = blogObject.map((blog) => blog.save())
-  await Promise.all(promiseArray)
+  await Blog.insertMany(helper.initBlogs)
 })
 
-test('4.8 blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('1. GET test suite', () => {
+  test('4.8 blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('verify number of blogs', async () => {
+    const blogs = await api.get('/api/blogs')
+    expect(blogs.body).toHaveLength(helper.initBlogs.length)
+  })
+
+  test('specific data is present or not', async () => {
+    const blogs = await api.get('/api/blogs')
+    expect(blogs.body.map((b) => b.title)).toContain('First class tests')
+  })
 })
 
-test('4.9 to verify unique identifier property', async () => {
-  const blogsFromDb = await helper.blogsInDb()
-  expect(blogsFromDb[0].id).toBeDefined()
-  expect(blogsFromDb[0]._id).not.toBeDefined()
+describe('2. POST test suite', () => {
+  test('4.10 blog is posted to db', async () => {
+    const newBlog = {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const res = await helper.blogsInDb()
+    expect(res).toHaveLength(helper.initBlogs.length + 1)
+
+    expect(res.map((r) => r.title)).toContain(
+      'Go To Statement Considered Harmful'
+    )
+    expect(res.map((r) => r.author)).toContain('Edsger W. Dijkstra')
+  })
 })
 
-test('4.10 blog is posted to db', async () => {
-  const newBlog = {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  }
+describe('3. Test if Identifiers are present in added data', () => {
+  test('4.9 to verify unique identifier property', async () => {
+    const blogsFromDb = await helper.blogsInDb()
+    expect(blogsFromDb[0].id).toBeDefined()
+    expect(blogsFromDb[0]._id).not.toBeDefined()
+  })
+  test('4.11 is likes property present', async () => {
+    const newBlog = {
+      title: 'Checking if I am Liked',
+      author: 'Someone Unpredictable',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const res = await helper.blogsInDb()
-  expect(res).toHaveLength(helper.initBlogs.length + 1)
+    const res = await helper.blogsInDb()
+    expect(res).toHaveLength(helper.initBlogs.length + 1)
+    expect(res.find((b) => b.title === 'Checking if I am Liked').likes).toEqual(
+      0
+    )
+  })
 
-  expect(res.map((r) => r.title)).toContain(
-    'Go To Statement Considered Harmful'
-  )
-  expect(res.map((r) => r.author)).toContain('Edsger W. Dijkstra')
+  test('4.12 is title or url property missing', async () => {
+    const newBlogTitle = {
+      title: 'Checking if I have Title',
+      author: 'Someone Unpredictable',
+    }
+    const newBlogUrl = {
+      author: 'Someone Unpredictable',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    }
+
+    await api.post('/api/blogs').send(newBlogTitle).expect(400)
+    await api.post('/api/blogs').send(newBlogUrl).expect(400)
+
+    const res = await helper.blogsInDb()
+    expect(res).toHaveLength(helper.initBlogs.length)
+  })
 })
 
-test('4.11 is likes property present', async () => {
-  const newBlog = {
-    title: 'Checking if I am Liked',
-    author: 'Someone Unpredictable',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-  }
+describe('4. UPDATE test suite', () => {
+  test('check if likes are updated', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+    const initialLikes = blogToUpdate.likes
+    blogToUpdate.likes = initialLikes + 1
+    const blogAdded = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(blogToUpdate)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initBlogs.length)
 
-  const res = await helper.blogsInDb()
-  expect(res).toHaveLength(helper.initBlogs.length + 1)
-  expect(res.find((b) => b.title === 'Checking if I am Liked').likes).toEqual(0)
+    expect(blogAdded.body.updatedBlog.likes).toEqual(initialLikes + 1)
+  })
+
+  test('when no data is provided for update', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send({})
+      .expect(400)
+  })
+
+  test('when bad/wrong id provided for update', async () => {
+    await api.put('/api/blogs/123').send({ likes: 587 }).expect(400)
+  })
 })
 
-test('4.12 is title or url property missing', async () => {
-  const newBlogTitle = {
-    title: 'Checking if I have Title',
-    author: 'Someone Unpredictable',
-  }
-  const newBlogUrl = {
-    author: 'Someone Unpredictable',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-  }
+describe('5. DELETE test suite', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  await api.post('/api/blogs').send(newBlogTitle).expect(400)
-  await api.post('/api/blogs').send(newBlogUrl).expect(400)
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
 
-  const res = await helper.blogsInDb()
-  expect(res).toHaveLength(helper.initBlogs.length)
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initBlogs.length - 1)
+
+    const contents = blogsAtEnd.map((r) => r.title)
+
+    expect(contents).not.toContain(blogToDelete.title)
+  })
 })
-
 afterAll(async () => {
   await mongoose.connection.close()
 })
